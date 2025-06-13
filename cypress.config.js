8706eb9d55112581693d12735aa0dcf2d9c7c3f8
Ricalinea Nascimento <105435538+ricalinea@users.noreply.github.com>
2025-06-13 19:45:04 -0300

    FInalizada a seção 9

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     viewportHeight: 880,
     viewportWidth: 1280,
     supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    video: true
   }
 })
--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,4 +1,4 @@
-it('testa a página da política de privacidade de forma independente', () => {
+it.only('testa a página da política de privacidade de forma independente', () => {
   cy.visit('./src/privacy.html')
 
   cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
